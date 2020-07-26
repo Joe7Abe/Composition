fab691b80ce0e1339dd4f77bd02307bc5045f2d3
Joseph Abraham <josephabraham@Abrahams-iMac.local>
2020-07-26 01:50:07 -0400

    Initial commit

---
+++ src/academy/learnprogramming/Dimensions.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Dimensions {
+    private int widths;
+    private int heights;
+    private int depth;
+
+    public Dimensions(int widths, int heights, int depth) {
+        this.widths = widths;
+        this.heights = heights;
+        this.depth = depth;
+    }
+
+    public int getWidths() {
+        return widths;
+    }
+
+    public int getHeights() {
+        return heights;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+}
---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,22 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("2290","Dell","240",dimensions);
+
+        Monitor theMonitor = new Monitor("27in Beast","Dell",27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200","Asus",4,6,"v1.44");
+
+        PC thePC = new PC(theCase,theMonitor,theMotherboard);
+
+        thePC.getMonitor().drawPixelAt(1500,1200,"red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+
+
+    }
+}
